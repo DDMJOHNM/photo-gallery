22001a6d25395139dc009dd92feffe0691c2ccab
john <johnskag12@gmail.com>
2020-02-05 15:39:37 +1300

    added signup form

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import(
 	)
 
 
-var homeView *views.View
-var contactView *views.View
+var (
+	homeView *views.View
+ 	contactView *views.View
+	signupView *views.View
+	)
 
 func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","text/html")
@@ -21,6 +24,12 @@ func contact(w http.ResponseWriter, r *http.Request){
 	must(contactView.Render(w,nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","text/html")
+	must(signupView.Render(w,nil))
+}
+
+
 func must(err error)  {
 	if err != nil{
 		panic(err)
@@ -31,12 +40,14 @@ func main (){
 
 	//var err error
 	homeView =	views.NewView("bootstrap","views/home.gohtml")
-	contactView =	views.NewView("bootstrap","views/contact.gohtml")
+	contactView = views.NewView("bootstrap","views/contact.gohtml")
+	signupView = views.NewView("bootstrap","views/signup.gohtml")
 
 	var h http.Handler = http.HandlerFunc(home)
 	r:= mux.NewRouter()
 	r.HandleFunc("/",home)
 	r.HandleFunc("/contact",contact)
+	r.HandleFunc("/signup",signup)
 	r.NotFoundHandler = h
 	http.ListenAndServe(":3000",r)
 }
