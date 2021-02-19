500d9b54430042c43e86b7946d002e3d1e2933bb
John Mason <jmason176@gmail.com>
2021-02-20 08:48:39 +1300

    password reset added

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -25,9 +25,12 @@ func parseValues(values url.Values, dst interface{}) error {
 }
 
 func parseURLParams(r *http.Request, dst interface{}) error {
+
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
 
 	return parseValues(r.Form, dst)
 }
+
+//pagr 686 - 726
